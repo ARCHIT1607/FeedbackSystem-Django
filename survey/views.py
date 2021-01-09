452aaa2c145f3b42d010794ed0be2c83619cd751
ARCHIT1607 <archit778@gmail.com>
2021-01-09 17:55:32 +0530

    register and login completed

--- survey/views.py
+++ survey/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("success")
+    return render(request, "survey/index.html")
+
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'survey/register.html', context)
+
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'survey/login.html')
